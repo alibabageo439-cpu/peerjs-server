cf4b19c550c7af9a513c1b05a177f0ccc5356b0f
alibabageo439-cpu <alibabageo439@gmail.com>
2026-03-11 17:48:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const { PeerServer } = require('peer');
 const express = require('express');
-const app = express();
+const { ExpressPeerServer } = require('peer');
 
+const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
-  res.send('PeerJS Server Running! ✅');
+  res.send('PeerJS Server Running!');
 });
 
-const peerServer = PeerServer({
-  port: PORT,
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const peerServer = ExpressPeerServer(server, {
   path: '/',
   allow_discovery: true,
-  proxied: true,
-  alive_timeout: 60000,
   key: 'peerjs',
-  concurrent_limit: 5000,
-  cleanup_out_msgs: 1000,
 });
 
+app.use('/peerjs', peerServer);
+
 peerServer.on('connection', (client) => {
-  console.log('Client connected:', client.getId());
+  console.log('Connected:', client.getId());
 });
 
 peerServer.on('disconnect', (client) => {
-  console.log('Client disconnected:', client.getId());
+  console.log('Disconnected:', client.getId());
 });
-
-console.log(`PeerJS server running on port ${PORT}`);
